a3b92cb11afca6cfa89cb8ae3c19ea3ab1f778a6
Zaidan Noor <zaidannoor06@gmail.com>
2022-11-16 14:32:44 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: "5b083d60d54d34a24c695136c7afa2e9",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+  DEFAULT_LANGUAGE: "en-us",
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
---
+++ src/scripts/sw.js
@@ -0,0 +1,36 @@
+import CacheHelper from './utils/cache-helper';
+ 
+// Daftar asset yang akan dicaching
+const assetsToCache = [
+  './',
+  './icons/maskable_icon.png',
+  './icons/maskable_icon_x48.png',
+  './icons/maskable_icon_x72.png',
+  './icons/maskable_icon_x96.png',
+  './icons/maskable_icon_x128.png',
+  './icons/maskable_icon_x192.png',
+  './icons/maskable_icon_x384.png',
+  './icons/maskable_icon_x512.png',
+  './index.html',
+  './favicon.png',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js',
+];
+
+self.addEventListener("install", (event) => {
+
+  // TODO: Caching App Shell Resource
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
+});
+
+self.addEventListener("activate", (event) => {
+
+  // TODO: Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
+});
+
+self.addEventListener("fetch", (event) => {
+  // TODO: Add/get fetch request to/from caches
+  event.respondWith(CacheHelper.revalidateCache(event.request));
+});
